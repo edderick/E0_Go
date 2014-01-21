186deb761d98fa00f02a95e81e1c53b91a86f613
Edward Seabrook <edderick@live.co.uk>
2014-01-21 12:13:42 +0000

    Used base 64 encoding for talking to the web server..

--- src/poster.go
+++ src/poster.go
@@ -4,11 +4,14 @@ import (
     "net/http"
     "fmt"
     "net/url"
+    "encoding/base64"
 )
 
 func main() {
-    resp, err := http.PostForm("http://127.0.0.1:6666/bar",  
-        url.Values{"msg": {"Hello, World!"}})
+    str := base64.StdEncoding.EncodeToString([]byte("Hello, playground"))
+
+    resp, err := http.PostForm("http://127.0.0.1:8000/keyStream?role=master",  
+        url.Values{"msg": { str  }})
     
     fmt.Println(resp)
     fmt.Println(err)
